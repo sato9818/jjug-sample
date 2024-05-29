01d9869624fa75e6943db286d7d4622cab741f08
Koki Sato <kouki9818@gmail.com>
2024-05-29 17:39:00 +0900

    Add Armeria Client

---
+++ src/main/java/SampleArmeriaClient.java
@@ -0,0 +1,37 @@
+import com.linecorp.armeria.client.grpc.GrpcClients;
+import com.linecorp.armeria.common.logging.LogLevel;
+import com.linecorp.armeria.common.logging.LogWriter;
+
+import io.grpc.StatusRuntimeException;
+import sample.UserServiceGrpc.UserServiceBlockingStub;
+import sample.UserServiceOuterClass.GetUserRequest;
+import sample.UserServiceOuterClass.GetUserResponse;
+
+public class SampleArmeriaClient {
+    public static void main(String[] args) {
+        final LogWriter logWriter = LogWriter.builder()
+                                             .requestLogLevel(LogLevel.INFO)
+                                             .successfulResponseLogLevel(LogLevel.INFO)
+                                             .failureResponseLogLevel(LogLevel.ERROR)
+                                             .build();
+
+        final UserServiceBlockingStub userService = GrpcClients.builder("http://127.0.0.1:6565/")
+//                                                               .decorator(LoggingClient.builder()
+//                                                                                       .logWriter(logWriter)
+//                                                                                       .newDecorator())
+                                                               .build(UserServiceBlockingStub.class);
+
+        final int id = 1;
+
+        final GetUserRequest getUserRequest = GetUserRequest.newBuilder()
+                                                            .setId(id)
+                                                            .build();
+
+        try {
+            final GetUserResponse getUserResponse = userService.getUser(getUserRequest);
+            System.out.println(getUserResponse);
+        } catch (final StatusRuntimeException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
